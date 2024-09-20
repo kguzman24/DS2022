1c212db57353992c66813c7c31dcddd53a5d8549
KarenG <krngzmn10@gmail.com>
2024-09-20 15:27:37 -0400

    third script done

--- my-work/Lab3/convert-bundle.sh
+++ my-work/Lab3/convert-bundle.sh
@@ -8,6 +8,7 @@ awk '!/^[[:space:]]*$/'  lab3_data.tsv
 # Use `tr` - "translate"
 tr '\t' ',' < lab3_data.tsv > lab3_data.csv
 
+#exclude header and start counting at row 1
 LINES=$(tail -n +3 lab3_data.csv | wc -l) 
 echo "Number of lines: $LINES"
 tar -czvf converted-archive.tar.gz lab3_data.csv
