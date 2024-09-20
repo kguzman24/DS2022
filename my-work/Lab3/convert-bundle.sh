acadaf53e50c372172e4cad3788d8c6474f3911e
KarenG <krngzmn10@gmail.com>
2024-09-20 14:48:42 -0400

    second script done

---
+++ my-work/Lab3/convert-bundle.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+wget https://s3.amazonaws.com/ds2002-resources/labs/lab3-bundle.tar.gz
+tar -xzvf lab3-bundle.tar.gz
+
+awk '!/^[[:space:]]*$/'  lab3_data.tsv
+
+# Use `tr` - "translate"
+tr '\t' ',' < lab3_data.tsv > lab3_data.csv
+
+LINES=$(tail -n +3 lab3_data.csv | wc -l) 
+echo "Number of lines: $LINES"
+tar -czvf converted-archive.tar.gz lab3_data.csv
